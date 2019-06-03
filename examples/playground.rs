aa617ff9b2b61d9584619ceada2a016e9049646a
Nathan Lilienthal <nathan@nixpulvis.com>
2019-06-02 23:50:52 -0400

    Start normalization.
    no... you're the conformist.

--- examples/playground.rs
+++ examples/playground.rs
@@ -16,13 +16,29 @@ fn main() {
     dbg!(parser.parse(r"\x.x y"));
     dbg!(parser.parse(r"\x.\y.y z"));
 
+    // Identity function.
+    let id = parser.parse(r"λx.x").unwrap();
+    dbg!(&id.to_s());
+
     // Make the Y combinator.
-    let ω = parser.parse(r"λx.(x x)");
-    let Ω = parser.parse(r"(λx.(x x)) (λx.(x x))");
-    let W = parser.parse(r"λf.λx. f x x");
-    let Y = parser.parse(r"λf.(λx.f (x x)) (λx.f (x x))");
-    dbg!(ω);
-    dbg!(Ω);
-    dbg!(W);
-    dbg!(Y);
+    let ω = parser.parse(r"λx.(x x)").unwrap();
+    dbg!(&ω.to_s());
+    let Ω = parser.parse(r"(λx.(x x)) (λx.(x x))").unwrap();
+    dbg!(&Ω.to_s());
+    let W = parser.parse(r"λf.λx. f x x").unwrap();
+    dbg!(&W.to_s());
+    let Y = parser.parse(r"λf.(λx.f (x x)) (λx.f (x x))").unwrap();
+    dbg!(&Y.to_s());
+
+    let multi = parser.parse(r"(λx.(x y)) (λy.(x y))").unwrap();
+    dbg!(&multi.to_s());
+
+    println!("\n\n\n");
+    dbg!(parser.parse(r"(\f.\x.(f x)) (\x.x)").unwrap().normalize());
+
+    println!("\n\n\n");
+    dbg!(parser.parse(r"(\f.\x.(f x)) (\x.x)").unwrap().free_variables());
+
+    println!("\n\n\n");
+    dbg!(parser.parse(r"\f.\x.(f (x x) f (x x)) y").unwrap().normalize().to_s());
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,28 +1,147 @@
-#![feature(box_syntax)]
+#![feature(non_ascii_idents, box_syntax, box_patterns)]
 
 #[macro_use]
 extern crate lalrpop_util;
 
-#[derive(Debug)]
+use std::collections::HashSet;
+
+macro_rules! map(
+    { $($value:expr),+ } => {
+        {
+            let mut m = ::std::collections::HashSet::new();
+            $(
+                m.insert($value);
+            )+
+            m
+        }
+     };
+);
+
+#[derive(Debug, Clone, PartialEq, Eq)]
 pub enum Expression {
     Var(Variable),
     Abs(Abstraction),
     App(Application),
 }
 
-#[derive(Debug)]
+impl Expression {
+    // TODO: Display trait.
+    pub fn to_s(&self) -> String {
+        match self {
+            Expression::Var(Variable(s)) => s.clone(),
+            Expression::Abs(Abstraction(Variable(id), body)) => {
+                format!("λ{}.{}", id, body.to_s())
+            },
+            Expression::App(Application(box e1, box e2)) => {
+                format!("({} {})", e1.to_s(), e2.to_s())
+            },
+        }
+    }
+
+    pub fn normalize(&self) -> Self {
+        match self {
+            Expression::Var(_) |
+            Expression::Abs(_) => self.clone(),
+            Expression::App(Application(box e1, box e2)) => {
+                match e1.normalize() {
+                    Expression::Abs(Abstraction(id, body)) => {
+                        body.substitute(&e2, &id).normalize()
+                    },
+                    e @ _ => {
+                        Expression::App(Application(box e, box e2.clone()))
+                    }
+                }
+            }
+        }
+    }
+
+    pub fn free_variables(&self) -> HashSet<Variable> {
+        match self {
+            Expression::Var(v) => map! { v.clone() },
+            Expression::Abs(Abstraction(id, body)) => {
+                body.free_variables().difference(&map! { id.clone() }).cloned().collect()
+            },
+            Expression::App(Application(e1, e2)) => {
+                e1.free_variables().union(&e2.free_variables()).cloned().collect()
+            }
+        }
+    }
+
+    pub fn replace(&self, old: &Variable, new: &Variable) -> Self {
+        match self {
+            Expression::Var(v) => {
+                Expression::Var(v.replace(old, new))
+            },
+            Expression::Abs(Abstraction(id, body)) => {
+                Expression::Abs(Abstraction(id.replace(old, new),
+                                            box body.replace(old, new)))
+            },
+            Expression::App(Application(e1, e2)) => {
+                Expression::App(Application(box e1.replace(old, new),
+                                            box e2.replace(old, new)))
+            }
+        }
+    }
+
+    pub fn substitute(&self, value: &Self, variable: &Variable) -> Self {
+        match self {
+            Expression::Abs(Abstraction(id, body)) => {
+                let free = dbg!(value.free_variables());
+                if id == variable {
+                    self.clone()
+                } else if !free.contains(id) {
+                    Expression::Abs(Abstraction(variable.clone(),
+                                                box body.substitute(value, variable)))
+                } else {
+                    let fresh = Variable("??".to_string());
+                    let new_body = body.replace(&variable, &fresh);
+                    Expression::Abs(Abstraction(fresh,
+                                                box new_body.substitute(value, variable)))
+                }
+            },
+            Expression::Var(id) => {
+                if id == variable {
+                    value.clone()
+                } else {
+                    self.clone()
+                }
+            },
+            Expression::App(Application(e1, e2)) => {
+                Expression::App(Application(box e1.substitute(value, variable),
+                                            box e2.substitute(value, variable)))
+            }
+        }
+    }
+}
+
+#[derive(Debug, Clone, Hash, PartialEq, Eq)]
 pub struct Variable(pub String);
 
-#[derive(Debug)]
+impl Variable {
+    fn to_s(&self) -> String {
+        self.0.clone()
+    }
+
+    fn replace(&self, old: &Variable, new: &Variable) -> Self {
+        if self.0 == old.0 {
+            Variable(new.0.clone())
+        } else {
+            self.clone()
+        }
+    }
+}
+
+#[derive(Debug, Clone, PartialEq, Eq)]
 pub struct Abstraction(pub Variable, pub Box<Expression>);
 
-#[derive(Debug)]
+#[derive(Debug, Clone, PartialEq, Eq)]
 pub struct Application(pub Box<Expression>, pub Box<Expression>);
 
 lalrpop_mod!(pub lambda);
 
 #[cfg(test)]
 mod tests {
+    use super::*;
     use crate::lambda::ExpressionParser;
 
     #[test]
@@ -43,5 +162,46 @@ mod tests {
         assert!(ExpressionParser::new().parse(r"x x").is_ok());
         assert!(ExpressionParser::new().parse(r"(x y)").is_ok());
         assert!(ExpressionParser::new().parse(r"(\x.x y)").is_ok());
+    }
+
+    #[test]
+    fn replace() {
+        let parser = ExpressionParser::new();
+        let a_var = Variable("a".into());
+        let b_var = Variable("b".into());
+
+        let a = parser.parse(r"a").unwrap();
+        let b = parser.parse(r"b").unwrap();
+        assert_eq!(b, a.replace(&a_var, &b_var));
+
+        let id = parser.parse(r"λa.a").unwrap();
+        let id2 = parser.parse(r"λb.b").unwrap();
+        assert_eq!(id2, id.replace(&a_var, &b_var));
+    }
+
+    // #[test]
+    // fn substitute() {
+    //     let parser = ExpressionParser::new();
+
+    //     let a = parser.parse(r"a").unwrap();
+    //     let id = parser.parse(r"λx.x").unwrap();
+    //     assert_eq!(a, id.substitute(&a));
+    //     let ω = parser.parse(r"λx.(x x)").unwrap();
+    //     let ids = parser.parse(r"((λx.x) (λx.x))").unwrap();
+    //     assert_eq!(ids, ω.substitute(&id));
+    // }
+
+    #[test]
+    fn normalize() {
+        let parser = ExpressionParser::new();
+
+        let odd = parser.parse(r"(\f.\x.(f x)) (\x.x)").unwrap();
+        let normal = odd.normalize();
+        println!("{}", odd.to_s());
+        println!("{}", normal.to_s());
+        assert_eq!(parser.parse(r"\x.x").unwrap(),
+                   parser.parse(r"(\f.\x.(f x)) (\x.x)").unwrap().normalize());
+        // assert_eq!(parser.parse(r"\x.x").unwrap(),
+        //            parser.parse(r"(\x. (x x)) (\x. (x x))").unwrap().normalize());
     }
 }
