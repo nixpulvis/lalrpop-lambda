90aeac580eac3af46c017601e82e79e092461dd5
Nathan Lilienthal <nathan@nixpulvis.com>
2019-06-07 20:00:26 -0400

    Refine the macros example.

--- examples/macros.rs
+++ examples/macros.rs
@@ -4,52 +4,35 @@
 extern crate lalrpop_lambda;
 
 fn main() {
+    // Both the short `λ!` macro, as well as the ASCII `abs!` macro.
+    println!("{} = {}", λ!{x.x}, abs!{x.x});
+
+    // Mix in rust bindings with lambda calculus macros using `{binding}`.
     let ω = abs!{x.app!(x,x)};
     // Doesn't do what you might want.
-    println!("{}", app!(x,ω));
+    println!("app!(x,ω): {}", app!(x,ω));
     // But this does ;)
-    println!("{} -> {}", app!({&ω},x), app!({&ω},x).normalize(false));
+    println!("app!({{&ω}},x): {} -> {}",
+             app!({&ω},x),
+             app!({&ω},x).normalize(false));
 
-    println!();
-    let id = λ!{x.x};
-    print!("id: {}", id);
-    let id = abs!{x.x};
-    println!(" = {}", id);
-
+    // An empty church numeral.
     let zero = λ!{f.λ!{x.x}};
     println!("0: {}", &zero);
+
+    // A single church numeral.
     let one = λ!{f.λ!{x.γ!(f, x)}};
     println!("1: {}", &one);
-    let two = λ!{f.λ!{x.γ!(f, γ!(f, x))}};
-    println!("2: {}", &two);
-    let three = λ!{f.λ!{x.γ!(f, γ!(f, γ!(f, x)))}};
-    println!("3: {}", &three);
 
-    let succ = λ!{n.λ!{f.λ!{x.γ!(f, γ!(n, γ!(f, x)))}}};
-    println!("succ: {}", &succ);
-    let add = λ!{m.λ!{n.λ!{f.λ!{x.γ!(m, γ!(f, γ!(n, γ!(f, x))))}}}};
-    println!("add:  {}", &add);
-
-    println!();
-    println!("(normalize (id one)) -> {}",
+    // Identity application.
+    let id = λ!{x.x};
+    println!("(id one): {} -> {}",
+             app!({&id}, {&one}),
              app!({&id}, {&one}).normalize(false));
 
-    println!("(add one one) -> {}",
-             app!(app!({&add}, {&one}), {&one}));
-    println!("(normalize (add one one)) -> {}",
-             app!(app!({&add}, {&one}), {&one}).normalize(false));
-
-    println!();
-    println!("(succ one) -> {}",
-             app!({&succ}, {&one}));
-    println!("(normalize (succ one)) -> {}",
-             app!({&succ}, {&one}).normalize(false));
-
-    println!();
-    println!("{}",
-             abs!{f.abs!{x.
-                 app!(var!(f),
-                      app!(app!(abs!{f.abs!{x.app!(var!(f), var!(x))}},
-                                var!(f)),
-                           var!(x)))}}.normalize(false));
+    let two = abs!{f.abs!{x.app!(var!(f),
+                                 app!(app!(abs!{f.abs!{x.app!(var!(f),
+                                                              var!(x))}},
+                                           var!(f)),var!(x)))}};
+    println!("two: {} -> {}", two, two.normalize(false));
 }
