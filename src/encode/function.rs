942386038fdf9629e7320e664195699e106e2dbb
Nathan Lilienthal <nathan@nixpulvis.com>
2019-06-08 17:05:55 -0400

    Fn call is beautiful.

--- src/encode/function.rs
+++ src/encode/function.rs
@@ -1,23 +1,25 @@
 use crate::{Expression, Abstraction};
 
+/// Function call support for an `Expression`.
+///
+/// ```
+/// # #![feature(box_syntax)]
+/// # #[macro_use]
+/// # extern crate lalrpop_lambda;
+/// # fn main() {
+/// assert_eq!(0u64, λ!{x.x}(0).into());
+/// assert_eq!(γ!(γ!(a,b),0), γ!(a,b)(0));
+/// # }
+/// ```
 impl<T> FnOnce<(T,)> for Expression
     where T: Into<Expression> +
              From<Expression>
 {
     // TODO: Return Option<T> here too.
-    type Output = T;
+    type Output = Expression;
 
-    extern "rust-call" fn call_once(self, _t: (T,)) -> T {
-        match self {
-            Expression::Abs(Abstraction(_id, box body)) => {
-                // TODO: resolve self with env { id => t.into() }.
-                body.into()
-            },
-            e @ _ => {
-                // TODO: resolve self.
-                e.into()
-            }
-        }
+    extern "rust-call" fn call_once(self, t: (T,)) -> Expression {
+        γ!({self},t.0.into())
     }
 }
 
@@ -25,7 +27,19 @@ impl<T> FnOnce<(T,)> for Expression
 #[cfg(test)]
 mod tests {
     #[test]
-    fn call() {
-        assert_eq!(1, abs!{x.x}(1));
+    fn var() {
+        let one = abs!{f.abs!{x.app!(f,x)}};
+        assert_eq!(app!(x,{one}), var!(x)(1));
+    }
+
+    #[test]
+    fn abs() {
+        assert_eq!(5u64, abs!{x.x}(5).into());
+    }
+
+    #[test]
+    fn app() {
+        let zero = abs!{f.abs!{x.x}};
+        assert_eq!(app!(app!(a,b),{zero}), app!(a,b)(0));
     }
 }
