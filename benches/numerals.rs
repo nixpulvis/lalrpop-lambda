160ffcb721b80d6b6c8698944261e9c718a8dc20
Nathan Lilienthal <nathan@nixpulvis.com>
2019-06-09 10:54:38 -0400

    Add benchmarks (so we don't get too far off)

---
+++ benches/numerals.rs
@@ -0,0 +1,24 @@
+#[macro_use]
+extern crate criterion;
+extern crate lalrpop_lambda;
+
+use criterion::{Criterion, Fun};
+use lalrpop_lambda::Expression;
+
+fn compare_benchmark(c: &mut Criterion) {
+    c.bench_function_over_inputs("native addition", |b, &n| {
+        b.iter(|| {
+            n + n
+        })
+    }, &[0,1,2,4,8,16,32]);
+
+    c.bench_function_over_inputs("λ-expression addition", |b, &n| {
+        b.iter(|| {
+            let e = Expression::from(*n);
+            u64::from(e.clone() + e)
+        })
+    }, &[0,1,2,4,8,16,32]);
+}
+
+criterion_group!(benches, compare_benchmark);
+criterion_main!(benches);
