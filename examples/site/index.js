1dd4fd0e701d22374fc99a433252002ea60b2684
Nathan Lilienthal <nathan@nixpulvis.com>
2019-06-22 15:27:39 -0400

    Show error per fn on the exp.

--- examples/site/index.js
+++ examples/site/index.js
@@ -29,7 +29,7 @@ import("./node_modules/lalrpop-lambda/lalrpop_lambda.js").then(wasm => {
           </p>
         );
       } else if (this.state.error) {
-        var display = <LambdaError message={this.state.error} />
+        var display = <LambdaParseError message={this.state.error} />
       } else {
         var display = <LambdaOutputs exp={this.state.expression} />
       }
@@ -46,31 +46,28 @@ import("./node_modules/lalrpop-lambda/lalrpop_lambda.js").then(wasm => {
 
   class LambdaOutputs extends React.Component {
     render() {
-      let exp = this.props.exp;
-      var outputs;
-      try {
-        outputs = [
-          { label: 'parse', value: exp ? exp.toString() : '' },
-          { label: 'app', value: exp ? exp.applicative(false).toString() : '' },
-          { label: 'cbv', value: exp ? exp.call_by_value().toString() : '' },
-          { label: 'norm', value: exp ? exp.normal(false).toString() : '' },
-          { label: 'cbn', value: exp ? exp.call_by_name().toString() : '' },
-          // TODO: Hybrid by-value and applicative.
-          { label: 'spine', value: exp ? exp.head_spine(false).toString() : '' },
-          // TODO: Hybrid head spine and normal.
-          { label: "= numeral", value: exp ? exp.toNumber().toString() : '' },
-          { label: "= bool", value: exp ? exp.toBool().toString() : '' },
-        ];
-      } catch(e) {
-        outputs = [];
-      }
+      let outputs = [
+        { label: 'parse', func: 'toString' },
+        { label: 'app',   func: 'applicative' },
+        { label: 'cbv',   func: 'call_by_value' },
+        { label: 'norm',  func: 'normal' },
+        { label: 'cbn',   func: 'call_by_name' },
+        // TODO: Hybrid by-func and applicative.
+        { label: 'spine', func: 'head_spine' },
+        // TODO: Hybrid head spine and normal.
+        { label: "= numeral", func: 'toNumber' },
+        { label: "= bool",    func: 'toBool' },
+      ];
       return (
         <table>
-          {outputs.map((o, i) => {
-            return (<LambdaOutput key={i}
-                                  label={o.label}
-                                  value={o.value} />);
-          })}
+          <tbody>
+            {outputs.map((o, i) => {
+              return (<LambdaOutput key={i}
+                                    label={o.label}
+                                    exp={this.props.exp}
+                                    func={o.func} />);
+            })}
+          </tbody>
         </table>
       )
     }
@@ -78,16 +75,27 @@ import("./node_modules/lalrpop-lambda/lalrpop_lambda.js").then(wasm => {
 
   class LambdaOutput extends React.Component {
     render() {
+      if (this.props.func) {
+        try {
+          var result = (<code>
+            {this.props.exp[this.props.func]().toString()}
+          </code>);
+        } catch(e) {
+          var result = <code className='error'>{e.toString()}</code>;
+        }
+      } else {
+        var result = '';
+      }
       return (
         <tr>
           <th>{this.props.label}</th>
-          <td><code>{this.props.value}</code></td>
+          <td>{result}</td>
         </tr>
       );
     }
   }
 
-  class LambdaError extends React.Component {
+  class LambdaParseError extends React.Component {
     render() {
       return (
         <p>
